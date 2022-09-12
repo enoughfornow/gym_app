ace8aa60afebbcea5c15b182d5524f99bc4211a7
Dmitry <enoughfornowbusiness@gmail.com>
2022-09-12 17:19:51 +0300

    change vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gym_app/',
   plugins: [react()],
   define: {
     'process.env': process.env,
